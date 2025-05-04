b2f2ab83b22e80cc50140eaf760e1a2c1de820b4
Purushotham1803 <422129@student.nitandhra.ac.in>
2025-05-04 12:16:32 +0530

    ft

--- downstream_task/retrieval/retrieval.py
+++ downstream_task/retrieval/retrieval.py
@@ -523,7 +523,7 @@ if __name__ == '__main__':
     # TODO: !!!!!!!!!!! MIMIC(val, test) or OPENI(val, test)
     parser.add_argument("--eval_len_size", type=int, default=354, choices=[759, 1536, 710, 354],
                         help="example size per idx_matching_example")  # 759
-    parser.add_argument("--do_train", type=bool, default=False, help="Train & Evaluate")
+    parser.add_argument("--do_train", type=bool, default=True, help="Train & Evaluate")
     parser.add_argument("--do_test", type=bool, default=True, help="Test")
 
     # eval_during_training
